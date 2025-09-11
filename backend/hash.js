daab623fac2f7a20ec42cdb2ed767fe6d57981c8
shaho-a <shahoalirezaei95@gmail.com>
2025-09-12 00:12:59 +0330

    test vercel

--- backend/hash.js
+++ backend/hash.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 
-const plainPassword = "932523023"; // رمزی که می‌خوای استفاده کنی
+const plainPassword = "932523023"; // 
 
 bcrypt.hash(plainPassword, 10).then((hash) => {
   console.log("Hashed password:", hash);
