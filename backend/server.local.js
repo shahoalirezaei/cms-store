25b2494bf6edc143578d63773add20b4245962a4
shaho-a <shahoalirezaei95@gmail.com>
2025-09-11 16:22:35 +0330

    vercel-ready: add server.local.js for local dev

---
+++ backend/server.local.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const app = require('./server');
+
+const port = process.env.PORT || 8001;
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
