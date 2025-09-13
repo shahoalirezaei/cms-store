c4a49d8462f14ceea97eed10a09e61370354ce56
shaho-a <shahoalirezaei95@gmail.com>
2025-09-14 00:36:57 +0330

    vercel-ready: backend > edit server.js for cros2

--- backend/server.js
+++ backend/server.js
@@ -21,13 +21,9 @@ const setupRouter = require("./routes/setupDemo");
 
 const app = express();
 
-// FRONTEND_URL دقیقا دامنه فرانت روی Vercel باشه
-const FRONTEND_URL = process.env.FRONTEND_URL;
+const FRONTEND_URL = process.env.FRONTEND_URL || "*";
 
-app.use(cors({
-  origin: FRONTEND_URL,  // فقط اجازه به دامنه فرانت
-  credentials: true      // مهم: اجازه ارسال هدر Authorization
-}));
+app.use(cors({ origin: FRONTEND_URL }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
